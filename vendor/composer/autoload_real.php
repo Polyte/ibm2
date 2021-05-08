49cb0ea9964734308addaad910419320c958c327
Polyt <phmakwala@gmail.com>
2021-05-08 17:24:49 +0200

    batch
    Last batch

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit85f2fbd8ebba86b0cd9c03d013c9e5e7
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit85f2fbd8ebba86b0cd9c03d013c9e5e7', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit85f2fbd8ebba86b0cd9c03d013c9e5e7', 'loadClassLoader'));
@@ -32,7 +34,7 @@ class ComposerAutoloaderInit85f2fbd8ebba86b0cd9c03d013c9e5e7
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit85f2fbd8ebba86b0cd9c03d013c9e5e7::getInitializer($loader));
         } else {
